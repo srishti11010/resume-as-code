ce680c3c99496d789b48a70b168547b5b3aff30a
Srishti Chhabra <srishti11010@gmail.com>
2025-06-20 21:49:23 +0200

    Updated renderer code as per revised template + added css

--- src/renderer.py
+++ src/renderer.py
@@ -7,4 +7,4 @@ env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 
 def render_html(resume_data: dict, theme: str = 'modern') -> str:
     template = env.get_template(f'{theme}.html.j2')
-    return template.render(resume=resume_data)
+    return template.render(**resume_data)
